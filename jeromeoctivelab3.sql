91e0de3057932e1c4a98ce30f1e0b80b19bb1cef
joctive <joctive@gmail.com>
2017-02-07 08:08:56 -0500

    Add files via upload

---
+++ jeromeoctivelab3.sql
@@ -0,0 +1,31 @@
+select ordNumber , totalUSD from Orders;
+
+select name, city from Agents
+where  name = ‘Smith’;
+
+select pid, name, priceUSD from Products
+where  quantity > 200100;
+
+select name, city from Customers
+where city = ‘Duluth’ ;
+
+select name from Products
+where city not in (‘New York’,  ‘Duluth’) ;
+
+select * from Agents 
+where city not in (‘Dallas’,  ‘Duluth’)  and priceUSD >=  1 ;
+
+select *
+from Orders
+where  month = ‘Feb’  or  month  =  ‘May’ ;
+
+select *
+from Orders
+where  month = ‘Feb’  and priceUSD  >=  600;
+
+select *
+from Orders
+where cid = ‘C005’ ;
+
+
+
